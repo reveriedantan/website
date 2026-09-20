890fd7f500f087402c2f21676a61951f81e73913
ju-none <julien.leclercqprof@gmail.com>
2026-09-20 02:54:05 +0200

    Updates

--- src/components/index.js
+++
@@ -1,4 +0,0 @@
-export { default as Banner } from "./Banner";
-export { default as Separator } from "./Separator";
-export { default as Footer } from "./Footer";
-export { default as Map } from "./Map";
